6f2bb6343e0a3ddf5d75db7037231a5ceff09508
saimohan <saimohan6056@gmail.com>
2025-12-02 10:27:42 +0530

    Added CORS changes

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'restapi',
+    'corsheaders', 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ➕ Minimal Working Example (Enable all CORS)
+CORS_ALLOW_ALL_ORIGINS = True
